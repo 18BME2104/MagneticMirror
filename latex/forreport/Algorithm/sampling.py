478a10780963fa606264caa35b2bc0a1f90bafc8
18BME2104 <kaushal.timilsina2018@vitstudent.ac.in>
2022-01-23 19:08:26 +0545

    defined some position and velocity samplers in sampling.ipynb and how to write to csv files

---
+++ latex/forreport/Algorithm/sampling.py
@@ -0,0 +1,19 @@
+    class Sampler:
+        #... something
+
+        def sample_uniform_position(self):
+            pass
+    
+        def sample_uniform_veclocity(self):
+            pass
+    
+        def sample_velocity_uniformKE(self):
+            pass
+    
+        def sample_Maxwellian_velocity(self):
+            pass
+    
+        def sample_parabolic_velocity(self):
+            pass
+
+        #... something else
